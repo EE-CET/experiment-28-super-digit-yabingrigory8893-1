8717003a78f3993b3332e0f7a6c5e8a8c8b441ce
yabingrigory8893 <yabingrigory3@gmail.com>
2026-04-10 01:58:36 -0700

    Update SuperDigit.java

--- src/SuperDigit.java
+++ src/SuperDigit.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
+
 public class SuperDigit {
-   
-        // TODO: Read n (as String or long) and k
-        // TODO: Calculate the initial sum of digits of n
-        // TODO: Multiply that sum by k to account for concatenation
-        // TODO: Find the super digit of the resulting value
-        // TODO: Print the result
-    
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            if (!sc.hasNext()) return;
+            String n = sc.next();
+            if (!sc.hasNextLong()) return;
+            long k = sc.nextLong();
+
+            long sumN = 0;
+            for (char c : n.toCharArray()) {
+                sumN += Character.getNumericValue(c);
+            }
+
+            long totalSum = sumN * k;
+            System.out.println(superDigit(totalSum));
+        }
+    }
+
+    private static long superDigit(long x) {
+        if (x < 10) return x;
+        long sum = 0;
+        while (x > 0) {
+            sum += x % 10;
+            x /= 10;
+        }
+        return superDigit(sum);
+    }
 }
